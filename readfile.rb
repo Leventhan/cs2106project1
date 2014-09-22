4dcd9cf9a9fadbf4617341613074ea5c78b82fd9
Yos Riady <yosriady@gmail.com>
2014-09-22 10:35:44 +0800

    Ignore empty line

--- readfile.rb
+++ readfile.rb
@@ -5,7 +5,9 @@ load 'command.rb'
 p = ProcessManager.new
 File.open(ARGV[0], "r") do |infile|
     while (line = infile.gets)
-      # TODO: ignore empty line
+      next if line.empty?
+      # TODO: ignore empty line and test init case with test-GR sample
+
       command = line.chomp
       is_valid = Command.validate_commmand(command)
       if is_valid
